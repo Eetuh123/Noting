34a80afa9bb6e4d2a233c8a0eb6000a866c6812a
EetuH <eetuh25@gmail.com>
2025-04-30 00:17:54 +0300

    Added basic regex, Removed double endpoint, Added fallback for Blazor pages

---
+++ Noting/Models/RepEntry.cs
@@ -0,0 +1,20 @@
+﻿namespace Noting.Models
+{
+    public class RepEntry
+    {
+        public int? Count { get; set; }
+
+        public bool IsFailure => !Count.HasValue;
+
+        public RepEntry(int count)
+        {
+            Count = count;
+        }
+        public RepEntry()
+        {
+            Count = null;
+        }
+        public override string ToString() =>
+            IsFailure ? "failure" : Count.Value.ToString();
+    }
+}
---
+++ Noting/Services/ExerciseService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using MongoDB.Bson;
+using Noting.Models;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+namespace Noting.Services
+{
+    public class ExerciseService
+    {
+        [Authorize]
+        public Task SaveFromText(string rawText, ObjectId userId) 
+        {
+            var exercise = new Exercise
+            {
+                RawText = rawText.Trim(),
+                Date = DateTime.UtcNow,
+                UserId = userId,
+                Weight = 0,
+                NameTag = "Default",
+                Reps = new List<RepEntry>(),
+                Notes = string.Empty
+            };
+            var pattern = @"^(?<weight>\d+(?:\.\d+)?)(?:kg|lb)?\s+(?<exercise>\w+)\s+(?<sets>\d+)x(?<reps>\d+)$";
+            var m = Regex.Match(exercise.RawText, pattern, RegexOptions.IgnoreCase);
+
+            exercise.Weight = (int)Math.Round(double.Parse(m.Groups["weight"].Value));
+            exercise.NameTag = m.Groups["exercise"].Value;
+
+            int sets = int.Parse(m.Groups["sets"].Value);
+            int reps = int.Parse(m.Groups["reps"].Value);
+            exercise.Reps = Enumerable
+                .Range(0, sets)
+                .Select(_ => new RepEntry { Count = reps })
+                .ToList();
+
+            DatabaseManipulator.Save(exercise);
+
+            return Task.CompletedTask;
+
+        }
+    }
+}
