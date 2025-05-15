aae3f0593dcea3d88f1971e6c2f2ffead76f889e
EetuH <eetuh25@gmail.com>
2025-05-15 15:33:03 +0300

    Working animatied text label

---
+++ Noting/wwwroot/js/floating-labels.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("input").forEach((input) => {
+        const wrapper = input.closest('.relative');
+        const label = wrapper?.querySelector('label');
+
+        const updateLabel = () => {
+            if (!label) return;
+
+            if (input.value.trim() !== "") {
+                label.classList.add(
+                    "top-0",
+                    "-translate-y-3",
+                    "scale-75",
+                    "font-semibold",
+                    "tracking-wider",
+                );
+            } else {
+                label.classList.remove(
+                    "top-0",
+                    "-translate-y-3",
+                    "scale-75",
+                    "font-semibold",
+                    "tracking-wider",
+                );
+            }
+        };
+
+        updateLabel();
+
+        input.addEventListener("input", updateLabel);
+    });
+});
